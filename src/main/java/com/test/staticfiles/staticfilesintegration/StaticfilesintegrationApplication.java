c7d7353743805d2b3df6336baf1a66baced65b21
Mohit Singh <msakra000@gmail.com>
2024-06-09 23:14:19 +0530

    first commit

---
+++ src/main/java/com/test/staticfiles/staticfilesintegration/StaticfilesintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.test.staticfiles.staticfilesintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticfilesintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticfilesintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/staticfiles/staticfilesintegration/StaticfilesintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.staticfiles.staticfilesintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticfilesintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
